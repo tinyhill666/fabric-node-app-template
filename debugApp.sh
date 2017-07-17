e9c3b9698f3cba81bd3f4ed156ad59be26b8d1e7
Fabric <qiushaoxi@163.com>
2017-07-17 11:34:02 +0800

    first add

---
+++ debugApp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+function installNodeModules() {
+	echo
+	if [ -d node_modules ]; then
+		echo "============== node modules installed already ============="
+	else
+		echo "============== Installing node modules ============="
+		npm install
+	fi
+	echo
+}
+
+
+installNodeModules
+
+PORT=4000 node --debug app
---
+++ script/NodebatchTest.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2017 IBM All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+'use strict';
+var log4js = require('log4js');
+var logger = log4js.getLogger('SampleWebApp');
+var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+var util = require('util');
+var app = express();
+var expressJWT = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var bearerToken = require('express-bearer-token');
+var cors = require('cors');
+var config = require('./config.json');
+var helper = require('./app/helper.js');
+var channels = require('./app/create-channel.js');
+var join = require('./app/join-channel.js');
+var install = require('./app/install-chaincode.js');
+var instantiate = require('./app/instantiate-chaincode.js');
+var invoke = require('./app/invoke-transaction.js');
+var query = require('./app/query.js');
+var host = process.env.HOST || config.host;
+var port = process.env.PORT || config.port;
+
+var peers = ["localhost:7051","localhost:8051"];
+var chaincodeName = "mycc";
+var channelName = "mychannel";
+var fcn = "move";
+var username = "Jim"
+var orgname = "org1"
+
+for (var i = 0; i < 10; i++) {
+	var args = ["a"+i,"b"+i,"10"];
+	logger.info(args);
+	invoke.invokeChaincode(peers, channelName, chaincodeName, fcn, args, username, orgname)
+	.then(function(message) {
+		res.send(message);
+	});
+}
+
+
